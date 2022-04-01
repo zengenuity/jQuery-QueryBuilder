d98dc435de6f41aea37e96ff340ffd81fb8be25b
Wayne Eaker <wayne@zengenuity.com>
2022-04-01 15:00:21 -0400

    Dist with Select2 plugin

--- dist/js/query-builder.js
+++ dist/js/query-builder.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery QueryBuilder 2.6.2
- * Copyright 2014-2021 Damien "Mistic" Sorel (http://www.strangeplanet.fr)
+ * Copyright 2014-2022 Damien "Mistic" Sorel (http://www.strangeplanet.fr)
  * Licensed under MIT (https://opensource.org/licenses/MIT)
  */
 (function(root, factory) {
@@ -5114,6 +5114,67 @@ QueryBuilder.extend(/** @lends module:plugins.NotGroup.prototype */ {
         this.trigger('rulesChanged');
     }
 });
+
+
+/**
+ * @class Select2
+ * @memberof module:plugins
+ * @description Applies Select2 on filters and operators combo-boxes.
+ * @param {object} [options]
+ * @param {string} [options.container='body']
+ * @param {string} [options.style='btn-inverse btn-xs']
+ * @param {int|string} [options.width='auto']
+ * @param {boolean} [options.showIcon=false]
+ * @throws MissingLibraryError
+ */
+QueryBuilder.define('select2', function(options) {
+    if (!$.fn.select2) {
+        Utils.error('MissingLibrary', 'Select2 is required to use "select2" plugin.');
+    }
+
+    var Selectors = QueryBuilder.selectors;
+
+    var applySelect2Options = function(rule) {
+        var rule_options = Object.assign({}, options);
+        if (rule && rule.filter && rule.filter.data && rule.filter.data.select2) {
+            rule_options = Object.assign(rule.filter.data.select2, rule_options);
+        }
+        if (rule && rule.data && rule.data.valueLabel) {
+            rule_options.data = [
+                {
+                    id: rule.data.valueId,
+                    text: rule.data.valueLabel
+                }
+            ];
+        }
+        rule.$el.find(Selectors.rule_value).removeClass('form-control').select2(rule_options)
+    };
+
+    // init selectpicker
+    this.on('afterCreateRuleFilters', function(e, rule) {
+        rule.$el.find(Selectors.rule_filter).removeClass('form-control').select2(options);
+    });
+    this.on('afterCreateRuleOperators', function(e, rule) {
+        rule.$el.find(Selectors.rule_operator).removeClass('form-control').select2(options);
+    });
+    this.on('afterCreateRuleInput', function(e, rule) {
+        applySelect2Options(rule);
+    });
+
+    // update selectpicker on change
+    this.on('afterUpdateRuleFilter', function(e, rule) {
+        rule.$el.find(Selectors.rule_filter).select2(options);
+    });
+
+    this.on('afterUpdateRuleOperator', function(e, rule) {
+        rule.$el.find(Selectors.rule_operator).select2(options);
+    });
+
+    this.on('afterUpdateRuleInput', function(e, rule) {
+        applySelect2Options(rule);
+    });
+
+}, {});
 
 
 /**
